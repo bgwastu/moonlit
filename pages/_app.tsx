61de642f0d14202107c20bf3865d580517f0ac0f
Bagas Wastu <git@wastu.net>
2023-08-26 12:58:07 +0700

    remove tailwind & install mantine

---
+++ pages/_app.tsx
@@ -0,0 +1,27 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Moonlit</title>
+        <meta name="description" content="Your melancholy music players." />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'dark',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
+}
