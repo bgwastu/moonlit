ab36ce4c7e539451fd6d9b8d9b2185b955848dcf
Bagas Wastu <git@wastu.net>
2023-09-11 12:20:26 +0700

    fix cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from "@hono/node-server";
-import { Hono, HonoRequest } from "hono";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { validator } from "hono/validator";
 import * as ytdl from "ytdl-core";
 
@@ -10,6 +11,13 @@ function isYoutubeURL(url: string) {
 }
 
 const app = new Hono();
+app.use(
+  "*",
+  cors({
+    origin: ["http://localhost:5173"],
+    exposeHeaders: ["*"],
+  })
+);
 app.post(
   "/yt",
   validator("json", (val, c) => {
@@ -24,7 +32,6 @@ app.post(
     return ytdl
       .getInfo(url)
       .then((info) => {
-
         // check whether the video is available
         if (info.player_response.playabilityStatus.status !== "OK") {
           return c.json({ message: "Video is not available" }, 422);
@@ -32,23 +39,28 @@ app.post(
 
         // if the length of the video is more than 10 minutes, return error
         if (+info.videoDetails.lengthSeconds > 600) {
-          return c.json({ message: "Video is too long, maximum is 10 minutes" }, 422);
+          return c.json(
+            { message: "Video is too long, maximum is 10 minutes" },
+            422
+          );
         }
 
         const title = info.videoDetails.title
           .replace(" (Official Music Video)", "")
           .replace(" [Official Music Video]", "");
-      
+
         const stream = ytdl.downloadFromInfo(info, {
           filter: "audioonly",
         });
 
         return c.newResponse(stream, {
           headers: {
-            "content-type": "audio/mpeg",
-            "x-yt-title": encodeURI(title),
-            "x-yt-thumb": encodeURI(info.videoDetails.thumbnails[0]?.url) || "",
-          }
+            "Content-Type": "audio/mpeg",
+            "Title": encodeURI(title),
+            "Author": encodeURI(info.videoDetails.author.name),
+            "Thumbnail":
+              encodeURI(info.videoDetails.thumbnails[0]?.url) || "",
+          },
         });
       })
       .catch((e) => {
