59429fa99b6f16c6d7b59d01a81963523ec997bc
Bagas Wastu <git@wastu.net>
2023-09-18 10:51:04 +0700

    fix bug music on end (2)

--- src/app/player/page.tsx
+++ src/app/player/page.tsx
@@ -52,7 +52,7 @@ const getSongLength = (bufferDuration: number, playbackRate: number) => {
 
 function getFormattedTime(seconds: number) {
   const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = Math.ceil(seconds % 60);
+  const remainingSeconds = Math.floor(seconds % 60);
 
   const formattedMinutes = String(minutes).padStart(2, "");
   const formattedSeconds = String(remainingSeconds).padStart(2, "0");
