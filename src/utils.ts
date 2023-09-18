75beada2b2299e7bb8a7f11749ce26aa290e376a
Bagas Wastu <git@wastu.net>
2023-09-18 10:07:49 +0700

    fix bug m.youtube.com not defined as YouTube URL

--- src/utils.ts
+++ src/utils.ts
@@ -5,6 +5,6 @@ export const getSongLength = (bufferDuration: number, playbackRate: number) => {
 
 export function isYoutubeURL(url: string) {
   const youtubeRegex =
-    /^(https?:\/\/)?(www\.)?(youtube\.com\/watch\?v=|music\.youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+    /^(https?:\/\/)?(www\.)?(m\.)?(youtube\.com\/watch\?v=|music\.youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
   return youtubeRegex.test(url);
 }
