3bb68454878634369436a4f212692e7839b9b5f1
Bagas Wastu <git@wastu.net>
2023-09-29 09:35:28 +0700

    add proper footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,27 @@
 
 import LoadingOverlay from "@/components/LoadingOverlay";
 import {
+  ActionIcon,
   Anchor,
-  Box,
+  AppShell,
   Button,
   Center,
   Container,
   Divider,
   Flex,
+  Footer,
   Text,
   TextInput,
   rem,
+  useMantineTheme,
 } from "@mantine/core";
 import { Dropzone } from "@mantine/dropzone";
 import { useForm } from "@mantine/form";
 import { notifications } from "@mantine/notifications";
 import {
+  IconAt,
   IconBrandYoutube,
+  IconExternalLink,
   IconMusicCheck,
   IconMusicPlus,
   IconMusicX,
@@ -177,20 +182,74 @@ function YoutubeUpload() {
   );
 }
 
+function FooterSection() {
+  const theme = useMantineTheme();
+  return (
+    <Footer height={68}>
+      <Container size="sm" h="100%">
+        <Flex align="center" justify="space-between" h="100%">
+          <Flex direction="column">
+            <Anchor
+              size="sm"
+              color="subtle"
+              href="https://github.com/bgwastu/moonlit/issues"
+              target="_blank"
+            >
+              Feature Request & Bug Report <IconExternalLink size={14} />
+            </Anchor>
+          </Flex>
+          <Flex gap={4}>
+            <ActionIcon
+              component="a"
+              href="mailto:bagas@wastu.net?subject=Moonlit"
+              target="_blank"
+              size="lg"
+              color="dark"
+              title="GitHub"
+              variant="transparent"
+            >
+              <IconAt />
+            </ActionIcon>
+            <ActionIcon
+              component="a"
+              href="https://github.com/bgwastu/moonlit"
+              target="_blank"
+              size="lg"
+              color="dark"
+              title="GitHub"
+              variant="transparent"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="28"
+                height="28"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2Z"
+                ></path>
+              </svg>
+            </ActionIcon>
+          </Flex>
+        </Flex>
+      </Container>
+    </Footer>
+  );
+}
+
 export default function UploadPage() {
   const [loading] = useAtom(loadingAtom);
 
   return (
     <>
       <LoadingOverlay visible={loading.status} message={loading.message} />
-      <Box
-        style={{
-          position: "relative",
-        }}
-      >
-        <Container size="sm" p="xl" mt="5dvh">
+
+      <AppShell footer={<FooterSection />} fixed={false} mt={28}>
+        <Container size="sm" p="xl">
           <Flex direction="column" gap="xl">
-            <Center mb="lg">
+            <Center>
               <Flex gap={6} align="center">
                 <Icon size={24} />
                 <Text
@@ -209,12 +268,8 @@ export default function UploadPage() {
             <Divider label="OR" labelPosition="center" />
             <LocalUpload />
           </Flex>
-          <Text color="dimmed" mt="xl">
-            Found bugs or want to suggest features?{" "}
-            <Anchor href="mailto:bagas@wastu.net">Let us know</Anchor>
-          </Text>
         </Container>
-      </Box>
+      </AppShell>
     </>
   );
 }
